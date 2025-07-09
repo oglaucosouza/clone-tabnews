f554190421aac8340cba4e365e0f50db0de2abda
Glauco Souza <oglaucosouza@gmail.com>
2025-07-09 23:37:37 +0000

    adiciona script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementando o TabNews a partir do ZERO para o curso.dev , com o Filipe Deschamps.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Estou estudando e também te amo! ❤️ kkk</h1>
+  return <h1>Estou estudando e também te amo! ❤️ kkk</h1>;
 }
 
 export default Home;
